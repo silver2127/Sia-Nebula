e3b6b443e59503fac09abe17b9fe9eb8b592143a
silver2127 <jamesbeit@protonmail.com>
2020-06-30 11:41:24 -0400

    yeety

---
+++ README.md
@@ -0,0 +1,4 @@
+Sia-Nebula mod repository
+
+	If you want to ask to use any of the code for your own mod.
+	Ask for permision first. since some of the code is others and not ours.
---
+++ common/defines/01_defines_ai.lua
@@ -0,0 +1,9 @@
+NDefines.NAI.GENERATE_WARGOAL_THREAT_BASELINE = 0.0
+NDefines.NAI.FORCE_FACTOR_AGAINST_EXTRA_MINOR = 0.4			-- AI considers generating wargoals against minors below this % of force compared to themselves to get at a bigger enemy.
+NDefines.NAI.MAX_EXTRA_WARGOAL_GENERATION = 2				-- AI may want to generate wargoals against weak minors to get at larger enemy, but never more that this at any given time.
+NDefines.NAI.WARGOAL_GENERATION_STRENGTH_FACTOR = 1.5	-- Desire to generate wargoal effected negatevely if actor strength is less than this factor of target strength
+NDefines.NAI.DECLARE_WAR_RELATIVE_FORCE_FACTOR = 0.4	-- Weight of relative force between nations that consider going to war
+NDefines.NAI.DECLARE_WAR_NOT_NEIGHBOR_FACTOR = 10.0		-- Multiplier applied before force factor if country is not neighbor with the one it is considering going to war
+NDefines.NAI.MIN_ANTAGONIZE_FOR_WARGOAL_JUSTIFICATION = -10000	-- AI countries will not fabricate claims against countries with an antagonization value lower than this.
+NDefines.NPolitics.BASE_LEADER_TRAITS = 3
+NDefines.NCountry.BASE_RESEARCH_SLOTS = 2
